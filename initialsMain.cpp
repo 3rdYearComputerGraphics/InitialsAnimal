7b5e1fb2db4fe32be78d5410da19b2caed9d3469
Lewis McLean <mclean.lewis@gmail.com>
2011-11-16 14:30:35 +0000

    added lines and keyboard shortcuts to use wireframe mode and automatic rotation

--- initialsMain.cpp
+++ initialsMain.cpp
@@ -2,7 +2,7 @@
 //  initialsMain.cpp
 //  Computer Graphics Initials
 //
-//  Created by Jamie Johnstone on 28/09/2011.
+//  Created by Jamie Johnstone & Lewis McLean on 28/09/2011.
 //  Copyright 2011 Heriot-Watt University. All rights reserved.
 //
 //  Assignment to create our initials out cubes and 3dcurves.
@@ -23,6 +23,15 @@
 #include "drawM.h"
 #include "jellyBodyShape.h"
 
+//======================================================
+// GLOBAL VARIABLES 
+//======================================================
+float pitch = 0.0f;
+float yaw = 0.0f;
+float pitch0, yaw0;
+bool MousePressed;
+int mouseX0, mouseY0;
+bool rotating=false;
 
 //======================================================
 //Prototypes
@@ -33,6 +42,116 @@ void displayCallBack(void);
 void executeViewControl(float y, float p);
 void viewControl();
 void drawInitials();
+void keyboardCallBack(unsigned char key, int x, int y) ;
+void rotateView(bool r);
+void idleCallBack ();
+void resetView();
+void drawAxesAndGridLines(bool x_y_display, bool y_z_display,  bool x_z_display);
+
+//======================================================
+// DRAW AXES and GRIDS
+//======================================================
+void drawAxesAndGridLines(bool x_y_display, bool y_z_display,  bool x_z_display)
+{
+	float offset;
+	glBegin(GL_LINES);
+    glColor3f(1, 0, 0);
+    glVertex3f(-20, 0, 0);					
+    glVertex3f(+20, 0, 0);					
+    glVertex3f( 0 ,-20, 0);				    	
+    glVertex3f(	0, +20, 0);
+    glVertex3f( 0, 0,-20);				    	
+    glVertex3f(	0, 0, +20);
+    
+	glEnd();
+	
+	glLineStipple(1, 0xAAAA); //line style = fine dots
+	glEnable(GL_LINE_STIPPLE);
+    
+	glBegin(GL_LINES);
+    
+    if (x_y_display) {glColor3f(0.0,0.7,0.7);
+		for (offset=-10.0;offset<10.1;offset++){
+			//draw lines in x-y plane
+			glVertex3f(-10.0, offset, 0.0);					// Top Left
+			glVertex3f(+10.0, offset, 0.0);					// Top Right
+			glVertex3f( offset,-10, 0.0);					// Bottom Right
+			glVertex3f(	offset,+10.0, 0.0);					// Bottom Left
+		}}
+    
+    if (y_z_display) {glColor3f(0.7,0.0,0.7);
+		for (offset=-10.0;offset<10.1;offset++){
+			//draw lines in y-z plane
+			glVertex3f( 0, offset, -10);					
+			glVertex3f(	0, offset, 10.0);
+			glVertex3f( 0, -10, offset);					
+			glVertex3f(	0, 10, offset);
+		}}
+    
+    if (x_z_display) {glColor3f(0.7,0.7,0.0);
+		for (offset=-10.0;offset<10.1;offset++){
+			//draw lines in x-z plane
+			glVertex3f( offset, 0, -10);					
+			glVertex3f(	offset, 0, 10.0);
+			glVertex3f( -10, 0, offset);					
+			glVertex3f(	10, 0, offset);
+		}}
+    
+	glEnd();
+	glDisable(GL_LINE_STIPPLE);
+    
+}
+
+//======================================================
+// VIEW CONTROL ROUTINES
+//======================================================
+
+void idleCallBack (){
+	yaw=yaw+.25;
+    glutPostRedisplay();
+}
+
+void rotateView(bool r){
+	rotating = r;
+	if (r) glutIdleFunc(idleCallBack); else glutIdleFunc(NULL);
+}
+
+void resetView(){
+	rotateView(false); //Stop view rotation
+	yaw=pitch=0;
+}
+
+
+//======================================================
+// KEYBOARD CALLBACK ROUTINE 
+//======================================================
+void keyboardCallBack(unsigned char key, int x, int y) {
+	printf("Keyboard call back: key=%c, x=%d, y=%d\n", key, x, y);
+	switch(key)
+	{
+        case 'b': case 'B':
+            glPolygonMode(GL_BACK,GL_FILL);
+            break;
+        case 'f': case 'F':
+            glPolygonMode(GL_FRONT,GL_FILL);
+            break;
+        case 'l': case 'L':
+            glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+            break;
+        case 'r': 
+            rotating= !rotating;
+            rotateView(rotating);
+            break;
+        case 'R':
+            resetView();
+            break;
+        default:
+            printf("Press b - back fill; f - front fill; l - line; i - increment; or d - decrement; r - rotate; R - reset view\n");
+	}
+    
+	glutPostRedisplay();
+}
+
 
 //======================================================
 //  GLOBAL VARIABLES
@@ -42,11 +161,6 @@ void drawInitials();
 static float G_theta[3]; // View X,Y,Z
 static float G_zoom=0.6;
 
-bool MousePressed; // Used in Mouse Drag to change the Viewpoint
-float pitch = 0.0f;
-float yaw = 0.0f;
-float pitch0, yaw0;
-int mouseX0, mouseY0;
 
 
 //=====================================================
@@ -180,6 +294,7 @@ void displayCallBack(void)
     
     //jellyfish body shape
     jellyBodyShape();
+    drawAxesAndGridLines(true, true, true);
 
 	glFlush();
 	glutSwapBuffers();
@@ -203,7 +318,7 @@ int main(int argc, char** argv)
 	glutIdleFunc(NULL); // Starts the Idle Function as having no routine attached to it. This is modified in rotateView()
 	glutMouseFunc(mouseClickCallBack);
     glutMotionFunc(mouseMotionCallBack);
-	//glutKeyboardFunc(keyboardCallBack);
+	glutKeyboardFunc(keyboardCallBack);
     
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glColor3f(1.0, 0.0, 0.0);
